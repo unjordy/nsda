3961c47101d28e080c5e9434925bd5ec7f2dd2ca
Jordan Mulcahey <snhjordy@gmail.com>
2019-07-22 16:29:09 -0700

    Remove unnecessary import

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 // Server API-related functions
 
 const fetch = require("node-fetch");
-const util = require("util");
 
 module.exports.json = url => fetch(url)
     .then(response => response.json());
